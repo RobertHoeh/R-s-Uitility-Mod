9c5479631719b21e3d0e3bc62984dd3cd8e054c4
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-10-26 16:28:14 -0400

    gave condensed dirt a polymer texture

--- src/main/kotlin/com/rsutils/RsUtilModServerside.kt
+++ src/main/kotlin/com/rsutils/RsUtilModServerside.kt
@@ -3,6 +3,7 @@ package com.rsutils
 import com.rsutils.block.ModBlocks
 import com.rsutils.item.ModItems
 import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils
 import net.fabricmc.api.ModInitializer
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
@@ -15,25 +16,24 @@ import org.slf4j.LoggerFactory
 
 object RsUtilModServerside : ModInitializer {
     const val MOD_ID: String = "rsutils"
-    private val logger = LoggerFactory.getLogger(MOD_ID)
+    val logger = LoggerFactory.getLogger(MOD_ID)
 
     val itemGroupKey: RegistryKey<ItemGroup> = RegistryKey<ItemGroup>.of(
         Registries.ITEM_GROUP.key,
         Identifier.of(MOD_ID, "item_group")
     )
 
-    val itemGroup = PolymerItemGroupUtils.builder()
+    val itemGroup: ItemGroup = PolymerItemGroupUtils.builder()
         .icon { ItemStack(ModItems.SUSPICIOUS_SUBSTANCE) }
         .displayName(Text.translatable("itemGroup.rsutils"))
         .build()
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		logger.info("Hello Fabric world From Server")
 
         PolymerItemGroupUtils.registerPolymerItemGroup(Identifier.of(MOD_ID, "item_group"), itemGroup)
+        PolymerResourcePackUtils.addModAssets(MOD_ID)
+        PolymerResourcePackUtils.markAsRequired()
 
         ModItems.initialize()
         ModBlocks.initialize()
--- src/main/kotlin/com/rsutils/block/CondensedDirt.kt
+++ src/main/kotlin/com/rsutils/block/CondensedDirt.kt
@@ -1,13 +1,26 @@
 package com.rsutils.block
 
+import com.rsutils.RsUtilModServerside
+import com.rsutils.mixin.SettingsAccessor
+import eu.pb4.polymer.blocks.api.BlockModelType
+import eu.pb4.polymer.blocks.api.PolymerBlockModel
+import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils
+import eu.pb4.polymer.blocks.api.PolymerTexturedBlock
 import eu.pb4.polymer.core.api.block.PolymerBlock
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
+import net.minecraft.util.Identifier
 import xyz.nucleoid.packettweaker.PacketContext
 
-class CondensedDirt(settings: Settings) : Block(settings), PolymerBlock {
+class CondensedDirt(settings: Settings) : Block(settings), PolymerTexturedBlock {
+    private val blockModel = PolymerBlockModel.of(
+        (this.settings as SettingsAccessor).registryKey.value.withPrefixedPath("block/")
+    )
+
+    val blockState = PolymerBlockResourceUtils.requestBlock(BlockModelType.FULL_BLOCK, blockModel)
+
     override fun getPolymerBlockState(state: BlockState?, context: PacketContext?): BlockState? {
-        return Blocks.COARSE_DIRT.getStateWithProperties(state)
+        return this.blockState
     }
 }
