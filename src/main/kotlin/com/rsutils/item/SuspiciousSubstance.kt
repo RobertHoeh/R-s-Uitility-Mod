3a6fa1e828682b2c758a67a8f7b51d674ee1c07d
Robertoman <92748186+RobertHoeh@users.noreply.github.com>
2025-10-26 10:03:48 -0400

    SuspiciousSubstance polymer item

--- src/main/kotlin/com/rsutils/item/SuspiciousSubstance.kt
+++ src/main/kotlin/com/rsutils/item/SuspiciousSubstance.kt
@@ -1,15 +1,19 @@
 package com.rsutils.item
 
 import com.rsutils.RsUtilModServerside
+import eu.pb4.polymer.core.api.item.SimplePolymerItem
 import net.minecraft.component.type.TooltipDisplayComponent
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.minecraft.item.tooltip.TooltipType
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
+import net.minecraft.util.thread.TaskQueue
+import xyz.nucleoid.packettweaker.PacketContext
 import java.util.function.Consumer
 
-class SuspiciousSubstance (settings: Item.Settings) : Item(settings) {
+class SuspiciousSubstance (settings: Item.Settings) : SimplePolymerItem(settings) {
     init{
 
     }
@@ -22,5 +26,9 @@ class SuspiciousSubstance (settings: Item.Settings) : Item(settings) {
     ) {
         textConsumer?.accept(Text.translatable("itemTooltip.${RsUtilModServerside.MOD_ID}.suspicious_substance")
             .formatted(Formatting.GOLD))
+    }
+
+    override fun getPolymerItem(itemStack: ItemStack?, context: PacketContext?): Item? {
+        return Items.GUNPOWDER
     }
 }
